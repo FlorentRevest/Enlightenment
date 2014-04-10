d5abfb7f5b39974e412229f5059a6c57649ea08b
Stefan Schmidt <s.schmidt@samsung.com>
2014-04-10 17:12:28 +0200

    e_client: Remove deleted client from UUID store
    Signed-off-by: Stefan Schmidt <s.schmidt@samsung.com>

--- src/bin/e_client.c
+++ src/bin/e_client.c
@@ -528,6 +528,11 @@ _e_client_free(E_Client *ec)
 
    e_hints_client_list_set();
    evas_object_del(ec->frame);
+
+#ifdef HAVE_WAYLAND_CLIENTS
+   e_uuid_store_entry_del(ec->uuid);
+#endif
+
    free(ec);
 }
 
